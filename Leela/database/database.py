784a910e22555e742f4494c808b2c88a99506db7
Evan <evan.lariviere@gmail.com>
2022-05-14 17:12:01 -0400

    folder structure

---
+++ Leela/database/database.py
@@ -0,0 +1,28 @@
+
+
+
+class Database:
+
+    @staticmethod
+    def build_database():
+        pass
+
+    @staticmethod
+    def delete_database():
+        pass
+
+    @staticmethod
+    def load_fannie_data():
+        pass
+
+    @staticmethod
+    def load_freddie_data():
+        pass
+
+    @staticmethod
+    def update_fannie_data():
+        pass
+
+    @staticmethod
+    def update_freddie_data():
+        pass
