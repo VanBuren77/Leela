815fa9e262d01c499d964b0e396a82724caa063b
Evan <evan.lariviere@gmail.com>
2022-05-15 11:31:02 -0400

    project skeleton

--- Leela/cli/commands/start_db.py
+++ Leela/cli/commands/start_db.py
@@ -3,10 +3,13 @@ import sys
 from leela.config import config
 import subprocess
 
-
 # Service needs to be running ->
 # postgresql-x64-14, if in stopped state, start it
+
+
+
 def start_db():
+
     print()
     print("------- Starting Postgres Server -------")
     print()
@@ -17,22 +20,8 @@ def start_db():
     # Check if service is running ->
 
     if os.name == "nt":
-        postgres_service_name = "postgresql-x64-14"
-
-        status = os.system(f"systemctl is-active --quiet {postgres_service_name}")
-        # will return 0 for active else inactive.
-        print(f"Service Status: {status}")
-
-        if status != 0:
-            # command = f"pg_ctl.ex start -N {postgres_service_name} -D \"C:\Program Files\PostgreSQL\9.5\data\" -w"
-            command = f"net start {postgres_service_name}"
-            p = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE)
-            p.communicate("")
-            # subprocess.call(['runas', '/user:Administrator', 'net start {postgres_service_name}'])
-            # os.system(f"net start {postgres_service_name}")
-        
-        # "C:/Program Files/PostgreSQL/14/data"
-        # os.system(f"pg_ctl start -D \"{config.POSTGRES_DIR}\"")
+        # postgres_service_name = "postgresql-x64-14"
+        os.system(f"pg_ctl start -D \"{config.POSTGRES_DIR}\"")
     else:
         print("Foo(): linux")
         pass
--- Leela/cli/commands/stop_db.py
+++ Leela/cli/commands/stop_db.py
@@ -4,4 +4,7 @@ from leela.config import config
 import os
 
 def stop_db():
+    print()
+    print("------- Stopping Postgres Server -------")
+    print()
     os.system(f"pg_ctl stop -D \"{config.POSTGRES_DIR}\"")
